02cd54f97ef801a4a1915371515012da9c028069
Joshua Hernandez <joshua113093@gmail.com>
2022-08-25 23:44:50 -0700

    API-Challenge

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "f1f8c2ed7cb2aa7631e2aa3a8def90e0"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBAndO_VpN6_zlNUHCwfKB_-m4V0Lz6UQc"
